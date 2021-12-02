8bdd493fdf4325b9007ac38f25e36136d35c4a84
Mahdi Hasnat Siyam <1705003@ugrad.cse.buet.ac.bd>
2021-12-02 17:36:53 +0600

    Offline Shell: Added cmd parsing

--- Operating System/Offline 1 Shell Script/solution.sh
+++ Operating System/Offline 1 Shell Script/solution.sh
@@ -1,33 +1,64 @@
 #!/bin/bash
 
-usage_message="$0 input_file_name [dir_name]"
+usage_message="usage:\"$0 input_file_name [dir_name]\""
 
-declare -A forbidden_extension
+declare -A forbidden_extensions
 
 read_extension(){
     # $1 -> input file name
     lines=`cat $1`
     for line in $lines;
     do
-        forbidden_extension["$line"]=1
+        forbidden_extensions["$line"]=1
     done < "$1"
-    echo "forbidden_ext ${!forbidden_extension[@]}"
+    echo "forbidden_ext ${!forbidden_extensions[@]}"
 }
 
+# check for proper command line arguments
 if (( $#  == 0 || $# > 2 ));
 then
     echo "${usage_message}"
-else
-    echo "fine"
-    if [ -f $1 ];
-    then
-        echo "fine1"
-        read_extension $1
-        echo "ext: ${!forbidden_extension[@]}"
-        
-
-    else
-        echo "First argument is not a file!"
-        echo "${usage_message}"
-    fi
+    exit 1
 fi
+
+echo "fine"
+
+# check if input file exist
+if [ ! -f "$1" ];
+then
+    echo "First argument is not a file!"
+    echo "${usage_message}"
+    exit 1
+fi
+
+echo "fine1"
+
+# check if input file is readable
+if [ ! -r "$1" ];
+then
+    echo "Read Permission not granted for: $1"
+    exit 1
+fi
+
+# read forbidden_extensionss 
+read_extension $1
+echo "ext: ${!forbidden_extensions[@]}"
+
+# define working_dir
+if (( $# >=2 ));
+then
+    working_dir=$2
+else
+    working_dir="."
+fi
+
+
+echo "wdir:${working_dir}"
+
+# check if working_dir exists
+if [ ! -d ${working_dir} ];
+then
+    echo "Working directory is invalid"
+    exit 1
+fi 
+
