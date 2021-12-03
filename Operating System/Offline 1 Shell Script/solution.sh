ce981554429f87a7159732fa0cf13ccde206619c
Mahdi Hasnat Siyam <1705003@ugrad.cse.buet.ac.bd>
2021-12-03 23:13:09 +0600

    Offline OS:Copied files

--- Operating System/Offline 1 Shell Script/solution.sh
+++ Operating System/Offline 1 Shell Script/solution.sh
@@ -7,9 +7,9 @@ declare -A forbidden_extensions
 read_extension(){
     # $1 -> input file name
     lines=`cat "$1"`
-    IFS=" \n\r\t"
-    while read line;
+    while read -r line;
     do
+        echo "line#$line"
         forbidden_extensions["$line"]=1
     done <<< "$lines"
     echo "forbidden_ext ${!forbidden_extensions[@]}"
@@ -25,6 +25,31 @@ is_allowed_extension(){
         return 0
     fi
 }
+
+
+set_output_dir(){
+    # $1 = working_dir
+    output_dir="$working_dir""/../output_dir/"
+
+    # check if output_dir already exist
+    if [ ! -d "$output_dir" ];
+    then
+        mkdir "$output_dir"
+        return 0
+    fi
+
+    n=1
+    output_dir="$working_dir""/../output_dir($n)/"
+    while [ -d "$output_dir" ];
+    do
+        n=$((n+1))
+        output_dir="$working_dir""/../output_dir($n)/"
+    done
+
+    mkdir "${output_dir}"
+
+}
+
 
 # check for proper command line arguments
 if (( $#  == 0 || $# > 2 ));
@@ -74,3 +99,85 @@ then
     exit 1
 fi 
 
+
+# set output_dir
+set_output_dir "${working_dir}"
+echo "Output dir is: $output_dir"
+
+
+# while read line
+# do
+#     echo "line: $line"
+#     if exist $line in forbidden_extensions;
+#     then
+#         echo "ok"
+#     else 
+#         echo "no"
+#     fi 
+# done 
+
+
+# tree "${working_dir}"
+
+
+# export -f is_allowed_extension
+
+# find "${working_dir}" -type f -print0
+
+# declare associative array for all files
+declare -A files
+
+
+add_file_to_output_dir(){
+    # $1 -> filePath
+    # $2 -> name
+    # $3 -> extension
+
+    echo "fileapth: $1 fileName: $2 fileExt: $3"
+
+    if [ -z "${files["$2"]}" ];
+    then
+        files["$2"]=1
+
+        targetDir="${output_dir}/$3"
+        target="$targetDir/$2"
+        targetDescription="$targetDir/desc_$3.txt"
+        echo "target: $target"
+
+        # create target dir
+        if [ ! -d "$targetDir" ];
+        then
+            mkdir -p "$targetDir"
+            touch "$targetDescription"
+        fi
+
+        cp -p "$1" "$target" # copy file
+        echo "$1" >> "$targetDescription"
+
+    fi
+
+}
+
+
+find "${working_dir}" -type f -print0 |
+while IFS= read -r -d '' filePath;
+do 
+    echo "${filePath}"
+    fileName="${filePath##*/}"
+    fileExtension="${fileName##*.}"
+    echo "fileName:$fileName fileExtension:$fileExtension"
+
+    
+
+    if [ "$fileName" != "$fileExtension" ];
+    then
+        # echo "ext found"
+        if is_allowed_extension "$fileExtension" ;
+        then
+            add_file_to_output_dir "$filePath" "$fileName" "$fileExtension"
+        fi
+    else
+        add_file_to_output_dir "$filePath" "$fileName" "others"
+    fi
+
+done
