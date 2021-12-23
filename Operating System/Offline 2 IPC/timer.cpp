7d5cba9bc09ece25cf576600d2e69e829b1db2a5
Mahdi Hasnat <mahdibuet3@gmail.com>
2021-12-23 21:14:16 +0600

    IPC Offline : Added timer skeleton

---
+++ Operating System/Offline 2 IPC/timer.cpp
@@ -0,0 +1,23 @@
+//
+// Created by mahdi on 12/23/2021.
+//
+
+#include "timer.h"
+#include <chrono>
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void timer_init() {
+
+}
+
+void timer_destroy() {
+
+}
+
+string timer_get_time_str() {
+
+}
---
+++ Operating System/Offline 2 IPC/timer.h
@@ -0,0 +1,18 @@
+//
+// Created by mahdi on 12/23/2021.
+//
+
+#ifndef OFFLINE_2_IPC_TIMER_H
+#define OFFLINE_2_IPC_TIMER_H
+
+#include<string>
+
+using namespace std;
+
+void timer_init();
+
+void timer_destroy();
+
+string timer_get_time_str();
+
+#endif //OFFLINE_2_IPC_TIMER_H
