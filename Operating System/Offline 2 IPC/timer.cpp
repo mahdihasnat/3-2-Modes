80aed20fa4e258c06be1c83d04ad085057e86270
= <=>
2021-12-28 19:22:30 +0600

    Offline IPC : Added timer impl

--- Operating System/Offline 2 IPC/timer.cpp
+++ Operating System/Offline 2 IPC/timer.cpp
@@ -9,9 +9,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using namespace chrono;
+
+time_point <system_clock> start;
 
 void timer_init() {
-
+    start = system_clock::now();
 }
 
 void timer_destroy() {
@@ -19,5 +22,20 @@ void timer_destroy() {
 }
 
 string timer_get_time_str() {
-
+    double milisec = duration_cast<milliseconds>(system_clock::now() - start).count();
+    cout << milisec << endl;
+    double sec = milisec / 1000.0;
+    stringstream ss;
+    ss << setprecision(3) << fixed << setw(6) << right << sec;
+    return ss.str();
 }
+
+// int main() {
+// 	timer_init();
+// 	for(int i=1;i<=2e8;i++){
+// 		int x;
+// 		x++;
+// 	}
+// 	cout<<timer_get_time_str()<<endl;
+// 	return 0;
+// }
